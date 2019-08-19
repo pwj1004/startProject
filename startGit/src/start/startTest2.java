1e0a8015d0b3f120128de090ea8cbd5f906a8785
mg194 <lotte_pwj@mz.co.kr>
2019-08-19 17:52:04 +0900

    test

--- startGit/src/start/startTest2.java
+++ startGit/src/start/startTest2.java
@@ -5,7 +5,7 @@ public class startTest2 {
 	public static void main(String[] args) {
 		
 		System.out.println("스타트");
-
+		System.out.println("스타트1.1");
 		System.out.println("스타트2");
 		System.out.println("스타트4");
 		System.out.println("스타트4.1");
